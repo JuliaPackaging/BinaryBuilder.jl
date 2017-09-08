909c1901b61af60363cb94237ddbac7aecbc73fc
Elliot Saba <staticfloat@gmail.com>
2017-09-08 01:01:36 -0700

    Update for `BinaryProvider` changes

--- test/build_libfoo_tarballs.jl
+++ test/build_libfoo_tarballs.jl
@@ -1,18 +1,20 @@
 #!/usr/bin/env julia
 using BinDeps2
 
-mkpath("./libfoo_tarballs")
+out_path = joinpath(pwd(), "libfoo_tarballs")
+rm(out_path; force=true, recursive=true)
+mkpath(out_path)
 
 for platform in supported_platforms()
     temp_prefix() do prefix
         cd("build_tests/libfoo") do
-            libfoo = LibraryResult(joinpath(libdir(prefix), "libfoo"))
-            fooifier = FileResult(joinpath(bindir(prefix), "fooifier"))
+            libfoo = LibraryProduct(prefix, "libfoo")
+            fooifier = ExecutableProduct(prefix, "fooifier")
             steps = [`make clean`, `make install`]
             dep = Dependency("foo", [libfoo, fooifier], steps, platform, prefix)
             build(dep; verbose=true)
         end
-    
+
         # Next, package it up as a .tar.gz file
         cd("./libfoo_tarballs") do
             rm("./libfoo_$(platform).tar.gz"; force=true)
