6ef992f157c3fdb9e8d3b00b3ddec382a63ac2c6
Keno Fischer <keno@juliacomputing.com>
2017-11-24 23:29:52 -0500

    Use squashfs on Travis
    This fixes #55 and should hopefully resolve all the instances of travis
    running out of disk space when using BinaryBuilder. While we're at it,
    move all the downloading to deps/build.jl, which fixes #42.
    
    Depends on https://github.com/staticfloat/julia-docker/pull/8

--- src/BinaryBuilder.jl
+++ src/BinaryBuilder.jl
@@ -14,19 +14,16 @@ include("AutoBuild.jl")
 include("Wizard.jl")
 
 function __init__()
-    global rootfs_tar, rootfs
-
+    global rootfs_base, rootfs
+    
     # If the user has overridden our rootfs tar location, reflect that here:
     def_dl_cache = joinpath(dirname(@__FILE__), "..", "deps", "downloads")
     downloads_cache = get(ENV, "BINARYBUILDER_DOWNLOADS_CACHE", def_dl_cache)
-    rootfs_tar = joinpath(downloads_cache, "rootfs.tar.gz")
+    rootfs_base = joinpath(downloads_cache, "rootfs")
 
     # If the user has overridden our rootfs unpack location, reflect that here:
-    def_rootfs_dir = joinpath(dirname(@__FILE__), "..", "deps", "root")
+    def_rootfs_dir = joinpath(dirname(@__FILE__),  "..", "deps", "root")
     rootfs = get(ENV, "BINARYBUILDER_ROOTFS_DIR", def_rootfs_dir)
-
-    # Initialize our rootfs and sandbox blobs
-    update_rootfs()
-    update_sandbox_binary()
 end
+
 end # module
