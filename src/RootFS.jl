eb26e903bf8e35b764d6d92d18c53ff6bb75822e
Elliot Saba <staticfloat@gmail.com>
2017-11-25 17:24:56 -0800

    Debug macOS unpacking

--- src/RootFS.jl
+++ src/RootFS.jl
@@ -169,7 +169,7 @@ function download_osx_sdk(;automatic::Bool = automatic_apple,
         "10.10" => (
             "$(urlbase)/MacOSX10.10.sdk.tar.xz",
             "4a08de46b8e96f6db7ad3202054e28d7b3d60a3d38cd56e61f08fb4863c488ce",
-            rootfs_dir("opt/x86_64-apple-darwin14/"),
+            rootfs_dir("opt/x86_64-apple-darwin14/MacOSX10.10.sdk"),
         ),
     )
     
@@ -216,7 +216,7 @@ function download_osx_sdk(;automatic::Bool = automatic_apple,
         end
     end
     
-    return download_verify_unpack(url, hash, dest, verbose=verbose, force=true)
+    return download_verify_unpack(url, hash, dirname(dest), verbose=verbose, force=true)
 end
 
 
