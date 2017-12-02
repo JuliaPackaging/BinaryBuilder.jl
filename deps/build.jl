e0cd0c64ec115394440cc4404c42a80841a1234e
Elliot Saba <staticfloat@gmail.com>
2017-12-01 22:21:57 -0800

    Actually just download the base image initially

--- deps/build.jl
+++ deps/build.jl
@@ -3,5 +3,5 @@ using BinaryBuilder
 # Build the sandbox binary
 BinaryBuilder.update_sandbox_binary()
 
-# Initialize just linux64, everything else will be downloaded on-demand
-BinaryBuilder.update_rootfs(triplet.([Linux(:x86_64)]); verbose=true)
+# Initialize just the base image, all shards will be downloaded on-demand
+BinaryBuilder.update_rootfs(String[]; verbose=true)
