9ef4b848df1d461d502ea7705802228c052da995
Elliot Saba <staticfloat@gmail.com>
2017-12-01 21:15:58 -0800

    Only download `linxu64` on `Pkg.build()`

--- deps/build.jl
+++ deps/build.jl
@@ -3,6 +3,5 @@ using BinaryBuilder
 # Build the sandbox binary
 BinaryBuilder.update_sandbox_binary()
 
-# Initialize just a few platforms here, let the others get brought in on demand
-t = triplet.([Linux(:x86_64), MacOS(:x86_64), Windows(:x86_64)])
-BinaryBuilder.update_rootfs(t; verbose=true)
+# Initialize just linux64, everything else will be downloaded on-demand
+BinaryBuilder.update_rootfs(triplet.([Linux(:x86_64)]); verbose=true)
