a453bcfc9618ee7f08affb3f56ecadb9d9506106
Elliot Saba <staticfloat@gmail.com>
2017-12-02 01:46:25 -0800

    Add background task to ensure Travis thinks we're still awake

--- src/AutoBuild.jl
+++ src/AutoBuild.jl
@@ -11,6 +11,19 @@ function autobuild(dir::AbstractString, src_name::AbstractString,
                    platforms::Vector, sources::Vector, script, products,
                    product_hashes::Dict = Dict();
                    verbose::Bool = true)
+    # If we're on Travis and we're not verbose, schedule a task to output a "." every minute so we don't die.
+    if haskey(ENV, "TRAVIS") && !verbose
+        run_travis_busytask = true
+        @async begin
+            # Don't let Travis think we're asleep...
+            info("Brewing a pot of coffee for Travis")
+            while run_travis_busytask
+                sleep(1)
+                print(".")
+            end
+        end
+    end
+
     # First, download the source(s), store in ./downloads/
     downloads_dir = joinpath(dir, "downloads")
     try mkpath(downloads_dir) end
@@ -63,6 +76,8 @@ function autobuild(dir::AbstractString, src_name::AbstractString,
         # Finally, destroy the build_path
         rm(build_path; recursive=true)
     end
+
+    run_travis_busytask = false
 end
 
 function print_buildjl(product_hashes::Dict)
