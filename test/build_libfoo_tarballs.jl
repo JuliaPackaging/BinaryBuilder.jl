af1688dfac959f1bc8be7c03c6b0257f2667061e
Elliot Saba <staticfloat@gmail.com>
2017-12-30 13:23:19 -0800

    Normalize `build_libfoo_tarballs.jl` a bit

--- test/build_libfoo_tarballs.jl
+++ test/build_libfoo_tarballs.jl
@@ -2,20 +2,35 @@
 using BinaryBuilder
 
 # First, package up the libfoo source so `autobuild()` can take it:
-try
-    mkdir("./downloads")
-end
 src_prefix = Prefix("./build_tests/libfoo")
 src_tarball, src_hash = package(src_prefix, "./downloads/libfoo"; verbose=true, force=true)
+
+# Our sources are local, that's fine
+sources = [
+    (src_tarball, src_hash)
+]
+
+# Choose which platforms to build for; if we've got an argument use that one,
+# otherwise default to just building all of them!
+build_platforms = supported_platforms()
+if length(ARGS) > 0
+    build_platforms = platform_key.(split(ARGS[1], ","))
+end
+info("Building for $(join(triplet.(build_platforms), ", "))")
+
+products = prefix -> [
+    LibraryProduct(prefix, "libfoo"),
+    ExecutableProduct(prefix, "fooifier")
+]
 
 # Build 'em!
 autobuild(
     pwd(),
     "libfoo",
-    supported_platforms(),
-    [(src_tarball, src_hash)],
-    "make clean; make install",
-    prefix -> [LibraryProduct(prefix, "libfoo"), ExecutableProduct(prefix, "fooifier")]
+    build_platforms,
+    sources,
+    "make install",
+    products,
 )
 
 # Cleanup temporary sources
