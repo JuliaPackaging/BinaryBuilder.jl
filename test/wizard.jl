67d00f3f1c2abcaeeb7b267c3ca0c0c16c2dd380
staticfloat@gmail.com <staticfloat@gmail.com>
2018-03-16 15:48:59 -0700

    Fix Wizard choking on broken symlinks
    Also add a test to make sure this doesn't happen in the future

--- test/wizard.jl
+++ test/wizard.jl
@@ -4,14 +4,30 @@ using VT100
 using BinaryBuilder
 using Compat.Test
 
+# Create fake terminal to communicate with BinaryBuilder over
 pty = VT100.create_pty(false)
+ins, outs = Base.TTY(pty.slave; readable=true), Base.TTY(pty.slave; readable=false)
+
+# Helper function to create a state, assign input/output streams, assign platforms, etc...
 function BinaryBuilder.WizardState(ins::Base.TTY, outs::Base.TTY)
     state = BinaryBuilder.WizardState()
     state.ins = ins
     state.outs = outs
     state.platforms = supported_platforms()
-    state
+    return state
 end
+
+# Helper function to try something and panic if it doesn't work
+do_try(f) = try
+    f()
+catch e
+    bt = catch_backtrace()
+    Base.display_error(stderr, e, bt)
+
+    # If a do_try fails, panic
+    Base.Test.@test false
+end
+
 
 # Test the download stage
 ## Tarballs
@@ -25,18 +41,6 @@ end
 HTTP.register!(r, "/*/source.tar.gz", HTTP.HandlerFunction(serve_tgz))
 server = HTTP.Server(r)
 @async HTTP.serve(server, ip"127.0.0.1", 14444; verbose=false)
-
-do_try(f) = try
-    f()
-catch e
-    bt = catch_backtrace()
-    Base.display_error(stderr, e, bt)
-
-    # If a do_try fails, panic
-    Base.Test.@test false
-end
-
-ins, outs = Base.TTY(pty.slave; readable=true), Base.TTY(pty.slave; readable=false);
 
 # Test one tar.gz download
 let state = BinaryBuilder.WizardState(ins, outs)
@@ -100,10 +104,42 @@ let state = BinaryBuilder.WizardState(ins, outs)
     wait(t)
 end
 
+# Test download with a broken symlink that used to kill the wizard
+# https://github.com/JuliaPackaging/BinaryBuilder.jl/issues/183
+let state = BinaryBuilder.WizardState(ins, outs)
+    # download tarball with known broken symlink
+    bsym_url = "https://github.com/staticfloat/small_bin/raw/d846f4a966883e7cc032a84acf4fa36695d05482/broken_symlink/broken_symlink.tar.gz"
+    bsym_hash = "470d47f1e6719df286dade223605e0c7e78e2740e9f0ecbfa608997d52a00445"
+    bsym_path = joinpath(tempspace, "broken_symlink.tar.gz")
+    download_verify(bsym_url, bsym_hash, bsym_path)
+    
+    state.step = :step3
+    state.source_urls = [bsym_url]
+    state.source_files = [bsym_path]
+    state.source_hashes = [bsym_hash]
+    t = @async do_try(()->BinaryBuilder.step34(state))
+
+    # If we get this far we've already won, follow through for good measure
+    write(pty.master, "mkdir -p \$WORKSPACE/destdir/bin\n")
+    write(pty.master, "cp /bin/bash \$WORKSPACE/destdir/bin/\n")
+    write(pty.master, "exit\n")
+    # We do not want to edit the script now
+    readuntil(pty.master, "Would you like to edit this script now?")
+    write(pty.master, "N\n")
+    readuntil(pty.master, "unique variable name for each build artifact:")
+    # Name the 'binary artifact' `bash`:
+    write(pty.master, "bash\n")
+
+    # Wait for the step to complete
+    wait(t)
+end
+
+
+# Clear anything waiting to be read on `pty.master`
 readavailable(pty.master)
 
-# Technically until the terminal has been put into raw mode
-# We could probably detect that, but good enough for now.
+# These technically should wait until the terminal has been put into/come out
+# of raw mode.  We could probably detect that, but good enough for now.
 wait_for_menu(pty) = sleep(1)
 wait_for_non_menu(pty) = sleep(1)
 
