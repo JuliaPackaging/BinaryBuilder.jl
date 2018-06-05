54ce39bbaaacf8182cffe1aaf912a3b9c18372fe
staticfloat@gmail.com <staticfloat@gmail.com>
2018-06-04 21:57:03 -0700

    Update `build_libfoo_tarballs.jl`

--- test/build_libfoo_tarballs.jl
+++ test/build_libfoo_tarballs.jl
@@ -1,13 +1,9 @@
 #!/usr/bin/env julia
 using BinaryBuilder
 
-# First, package up the libfoo source so `build_tarballs()` can take it:
-src_prefix = Prefix("./build_tests/libfoo")
-src_tarball, src_hash = package(src_prefix, "./downloads/libfoo"; verbose=true, force=true)
-
 # Our sources are local.  No biggie.
 sources = [
-    (src_tarball, src_hash)
+    "./build_tests/libfoo",
 ]
 
 # Build script is very complicated.
@@ -30,7 +26,3 @@ dependencies = [
 
 # Build the tarballs, and possibly a `build.jl` as well.
 build_tarballs(ARGS, "libfoo", sources, script, platforms, products, dependencies)
-
-# Cleanup temporary sources
-rm(src_tarball; force=true)
-rm("$(src_tarball).sha256"; force=true)
