3215f18ea9bdab5515fde3ebd5a6db70a64e26e0
Elliot Saba <staticfloat@gmail.com>
2019-08-30 17:03:27 -0700

    Fix `JLL` package bugs

--- src/AutoBuild.jl
+++ src/AutoBuild.jl
@@ -196,7 +196,6 @@ function build_tarballs(ARGS, src_name, src_version, sources, script,
             project,
             wrapper_tree_hash;
             registry=registry_url,
-            force_reset=false,
             cache=cache,
             push=true,
         )
@@ -863,19 +862,24 @@ function build_jll_package(src_name::String, build_version::VersionNumber, code_
         print(io, """
         module $(src_name)_jll
         using Pkg, Pkg.BinaryPlatforms, Pkg.Artifacts, Libdl
+        import Base: UUID
 
         # Load Artifacts.toml file
-        artifacts_toml = joinpath(@__DIR__, "Artifacts.toml")
+        artifacts_toml = joinpath(@__DIR__, "..", "Artifacts.toml")
 
         # Extract all platforms
-        artifacts = Pkg.Artifacts.load_artifacts_toml(artifacts_toml, $(repr(jll_uuid("$(src_name)_jll"))))
+        artifacts = Pkg.Artifacts.load_artifacts_toml(artifacts_toml; pkg_uuid=$(repr(jll_uuid("$(src_name)_jll"))))
         platforms = [Pkg.Artifacts.unpack_platform(e, $(repr(src_name)), artifacts_toml) for e in artifacts[$(repr(src_name))]]
         
         # Filter platforms based on what wrappers we've generated on-disk
-        platforms = filter(p -> isfile(joinpath(@__DIR__, "wrappers", triplet(p))), platforms)
+        platforms = filter(p -> isfile(joinpath(@__DIR__, "wrappers", triplet(p) * ".jl")), platforms)
 
         # From the available options, choose the best platform
         best_platform = select_platform(Dict(p => triplet(p) for p in platforms))
+
+        if best_platform === nothing
+            error("Unable to load $(src_name); unsupported platform \$(triplet(platform_key_abi()))")
+        end
         
         # Load the appropriate wrapper
         include(joinpath(@__DIR__, "wrappers", "\$(best_platform).jl"))
