0c71054245a79fae4736e114a6bc14af52eb6803
Elliot Saba <staticfloat@gmail.com>
2019-11-06 17:39:13 -0800

    Plumb more `io` kwargs for audit passes

--- src/auditor/symlink_translator.jl
+++ src/auditor/symlink_translator.jl
@@ -5,13 +5,13 @@ Walks through the root directory given within `root`, finding all symlinks that
 point to an absolute path within `root`, and rewriting them to be a relative
 symlink instead, increasing relocatability.
 """
-function translate_symlinks(root::AbstractString; verbose::Bool=false)
+function translate_symlinks(root::AbstractString; verbose::Bool=false, io::IO = stdout)
     for f in collect_files(root, islink)
         link_target = readlink(f)
         if isabspath(link_target) && startswith(link_target, "/workspace")
             new_link_target = relpath(link_target, replace(dirname(f), root => "/workspace/destdir"))
             if verbose
-                @info("Translating $f to point to $(new_link_target)")
+                info(io, "Translating $f to point to $(new_link_target)")
             end
             rm(f; force=true)
             symlink(new_link_target, f)
