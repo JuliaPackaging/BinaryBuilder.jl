c9df8014396f640b0808aab551b7a26b59831e67
Elliot Saba <staticfloat@gmail.com>
2019-11-15 16:52:10 -0800

    Explicitly shred `creds` to avoid warning

--- src/AutoBuild.jl
+++ src/AutoBuild.jl
@@ -179,11 +179,15 @@ function build_tarballs(ARGS, src_name, src_version, sources, script,
         if !isdir(code_dir)
             # If it does exist, clone it down:
             @info("Cloning wrapper code repo from https://github.com/$(deploy_repo) into $(code_dir)")
-            creds = LibGit2.UserPasswordCredential(
-                deepcopy(gh_username),
-                deepcopy(gh_auth.token),
-            )
-            LibGit2.clone("https://github.com/$(deploy_repo)", code_dir; credentials=creds)
+            try
+                creds = LibGit2.UserPasswordCredential(
+                    deepcopy(gh_username),
+                    deepcopy(gh_auth.token),
+                )
+                LibGit2.clone("https://github.com/$(deploy_repo)", code_dir; credentials=creds)
+            finally
+                Base.shred!(creds)
+            end
         end
     end
 
