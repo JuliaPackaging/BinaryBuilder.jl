d52d9bcd69563228e5d1df004b10cf6a5dbf0381
Elliot Saba <staticfloat@gmail.com>
2019-11-20 14:56:31 -0800

    Only update the registry once per session.
    We were on the verge of greatness, we were this close!

--- src/wizard/yggdrasil.jl
+++ src/wizard/yggdrasil.jl
@@ -21,11 +21,12 @@ end
 # We only want to update the registry once per run
 registry_updated = false
 function update_registry(ctx = Pkg.Types.Context())
+    global registry_updated
     if !registry_updated
         Pkg.Registry.update(ctx, [
             Pkg.Types.RegistrySpec(uuid = "23338594-aafe-5451-b93e-139f81909106"),
         ])
-        registry_updated
+        registry_updated = true
     end
 end
 
