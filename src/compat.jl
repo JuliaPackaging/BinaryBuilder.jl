9cfe850435ff707b552621d2336713ed398fdd59
Elliot Saba <staticfloat@gmail.com>
2019-11-20 15:01:48 -0800

    Fix `info()`/`@info()` discrepancy
    I have always been annoyed that `info()` looks different from `@info`,
    and our desire to be able to shuttle around IO objects means that we
    must use the former.  This at least causes us to fall back in the
    standard case where we actually want to see the messages, and
    simultaneously lets us use things like `@test_logs`.

--- src/compat.jl
+++ src/compat.jl
@@ -1,13 +1,21 @@
 ## This is until we have a good replacement for `info(io, msg)` on 0.7+
 function info(io::IO, args...)
-    printstyled(io, "Info: "; color=Base.info_color())
-    printstyled(io, args...; color=Base.info_color())
-    println(io)
+    if io != stdout
+        printstyled(io, "Info: "; color=Base.info_color())
+        printstyled(io, args...; color=Base.info_color())
+        println(io)
+    else
+        @info(args)
+    end
 end
 function warn(io::IO, args...)
-    printstyled(io, "Warning: "; color=Base.warn_color())
-    printstyled(io, args...; color=Base.warn_color())
-    println(io)
+    if io != stdout
+        printstyled(io, "Warning: "; color=Base.warn_color())
+        printstyled(io, args...; color=Base.warn_color())
+        println(io)
+    else
+        @warn(args)
+    end
 end
 
 function extract_kwargs(kwargs, keys)
