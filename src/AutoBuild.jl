11de68eaf6888049c05f5c013c710f077a4e46cd
Elliot Saba <staticfloat@gmail.com>
2019-12-05 16:40:35 -0800

    License JLL packages themselves as MIT
    Fixes https://github.com/JuliaPackaging/BinaryBuilder.jl/issues/472

--- src/AutoBuild.jl
+++ src/AutoBuild.jl
@@ -1,7 +1,7 @@
 export build_tarballs, autobuild, print_artifacts_toml, build
 import GitHub: gh_get_json, DEFAULT_API
 import SHA: sha256
-using Pkg.TOML
+using Pkg.TOML, Dates
 using RegistryTools, Registrator
 
 """
@@ -1186,6 +1186,19 @@ function build_jll_package(src_name::String, build_version::VersionNumber, code_
             run(`\$mungify_exe_path \$num_chars`)
         end
         ```
+        """)
+    end
+
+    # Generate LICENSE.md
+    open(joinpath(code_dir, "LICENSE.md"), "w") do io
+        print(io, """
+        Copyright $(year(now()))
+
+        Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+        The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+        THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
         """)
     end
 
