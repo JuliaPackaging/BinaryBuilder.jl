ac460d4b294c0adfb89ba75f0187d2c82431d89e
Elliot Saba <staticfloat@gmail.com>
2019-12-27 13:23:23 -0800

    Fix botched rebase

--- src/wizard/deploy.jl
+++ src/wizard/deploy.jl
@@ -2,7 +2,7 @@ function print_build_tarballs(io::IO, state::WizardState)
     urlhashes = zip(state.source_urls, state.source_hashes)
     sources_string = strip(join(map(urlhashes) do x
         string(repr(x[1])," =>\n    ", repr(x[2]), ",\n")
-    end,"\n    ")
+    end,"\n    "))
     if Set(state.platforms) == Set(supported_platforms())
         platforms_string = "supported_platforms()"
     else
