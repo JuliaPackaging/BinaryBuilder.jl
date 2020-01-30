ec044ddf35f62359ed66c1f92d7dc2bdcc82753a
Elliot Saba <staticfloat@gmail.com>
2020-01-29 17:05:19 -0800

    Eliminate testing print

--- src/AutoBuild.jl
+++ src/AutoBuild.jl
@@ -614,7 +614,6 @@ function autobuild(dir::AbstractString,
     if src_version.build != ()
         error("Will not build with a `src_version` that has a build number already specified!")
     end
-    @show src_version
 
     # We must prepare our sources.  Download them, hash them, etc...
     sources = download_sources(sources; verbose=verbose)
