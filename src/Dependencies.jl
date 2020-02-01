1aae07c776b5e19fa3bd819d87e1fbce5d10cc9d
Mosè Giordano <m.giordano@ucl.ac.uk>
2020-02-01 14:18:44 +0000

    Define alias for the type returned by Pkg.PackageSpec

--- src/Dependencies.jl
+++ src/Dependencies.jl
@@ -1,13 +1,16 @@
 export Dependency, BuildDependency
 
-abstract type AbstractDependency{T<:Union{Pkg.Types.PackageSpec,String}} end
+# Pkg.PackageSpec return different types in different Julia versions so...
+const PkgSpec = typeof(Pkg.PackageSpec(name="dummy"))
 
-struct Dependency{T<:Union{Pkg.Types.PackageSpec,String}} <: AbstractDependency{T}
+abstract type AbstractDependency{T<:Union{PkgSpec,String}} end
+
+struct Dependency{T<:Union{PkgSpec,String}} <: AbstractDependency{T}
     pkg::T
 end
 Dependency(d::AbstractString) = Dependency(String(d))
 
-struct BuildDependency{T<:Union{Pkg.Types.PackageSpec,String}} <: AbstractDependency{T}
+struct BuildDependency{T<:Union{PkgSpec,String}} <: AbstractDependency{T}
     pkg::T
 end
 BuildDependency(d::AbstractString) = BuildDependency(String(d))
@@ -16,21 +19,21 @@ getpkg(d::AbstractDependency) = d.pkg
 
 dep_name(x::AbstractDependency) = x
 dep_name(x::String) = Dependency(x)
-dep_name(x::Pkg.Types.PackageSpec) = Dependency(x.name)
+dep_name(x::PkgSpec) = Dependency(x.name)
 
 # compatibility for Julia 1.3-
 if VERSION < v"1.4"
     Pkg.Types.registry_resolve!(ctx::Pkg.Types.Context, deps) = Pkg.Types.registry_resolve!(ctx.env, deps)
 end
 
-pkgspecify(name::AbstractString) = Pkg.Types.PackageSpec(;name=name)
-pkgspecify(ps::Pkg.Types.PackageSpec) = ps
+pkgspecify(name::AbstractString) = PkgSpec(;name=name)
+pkgspecify(ps::PkgSpec) = ps
 pkgspecify(dependency::Dependency) = Dependency(pkgspecify(getpkg(dependency)))
 pkgspecify(dependency::BuildDependency) = BuildDependency(pkgspecify(getpkg(dependency)))
 
 # Wrapper around `Pkg.Types.registry_resolve!` which keeps the type of the
 # dependencies.  TODO: improve this
-function registry_resolve!(ctx, dependencies::Vector{<:AbstractDependency{Pkg.Types.PackageSpec}})
+function registry_resolve!(ctx, dependencies::Vector{<:AbstractDependency{PkgSpec}})
     resolved_dependencies = Pkg.Types.registry_resolve!(ctx, getpkg.(dependencies))
     for idx in eachindex(dependencies)
         dependencies[idx] = typeof(dependencies[idx])(resolved_dependencies[idx])
@@ -40,7 +43,7 @@ end
 
 function resolve_jlls(dependencies::Vector; ctx = Pkg.Types.Context(), outs=stdout)
     if isempty(dependencies)
-        return true, Dependency{Pkg.Types.PackageSpec}[]
+        return true, Dependency{PkgSpec}[]
     end
 
     # Don't clobber caller
