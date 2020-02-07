6104bffcfd4961a257033a23e976fff9e77e536a
Elliot Saba <staticfloat@gmail.com>
2020-02-06 16:37:01 -0800

    Upgrade RootFS for a better test suite

--- src/Rootfs.jl
+++ src/Rootfs.jl
@@ -418,7 +418,7 @@ consists of four shards, but that may not always be the case.
 """
 function choose_shards(p::Platform;
             compilers::Vector{Symbol} = [:c],
-            rootfs_build::VersionNumber=v"2020.01.30",
+            rootfs_build::VersionNumber=v"2020.02.06",
             ps_build::VersionNumber=v"2020.02.06",
             GCC_builds::Vector{GCCBuild}=available_gcc_builds,
             LLVM_builds::Vector{LLVMBuild}=available_llvm_builds,
