a64ba4a1c8f445862374c02f949542380a791a0b
Mosè Giordano <mose@gnu.org>
2020-04-13 16:50:16 +0100

    Setup dependencies in the wizard matching the current compiler ABI

--- src/AutoBuild.jl
+++ src/AutoBuild.jl
@@ -443,6 +443,51 @@ function register_jll(name, build_version, dependencies;
     end
 end
 
+"""
+    get_concrete_platform(platform::Platform, shards::Vector{CompilerShard})
+
+Return the concrete platform for the given `platform` based on the GCC compiler
+ABI in the `shards`.
+"""
+function get_concrete_platform(platform::Platform, shards::Vector{CompilerShard})
+    # We want to get dependencies that have exactly the same GCC ABI as the
+    # chosen compiler, otherwise we risk, e.g., to build in an environment
+    # with libgfortran3 a dependency built with libgfortran5.
+    # `concrete_platform` is needed only to setup the dependencies and the
+    # runner.  We _don't_ want the platform passed to `audit()` or
+    # `package()` to be more specific than it is.
+    concrete_platform = platform
+    gccboostrap_shard_idx = findfirst(x -> x.name == "GCCBootstrap" &&
+                                      x.target.arch == platform.arch &&
+                                      x.target.libc == platform.libc,
+                                      shards)
+    if !isnothing(gccboostrap_shard_idx)
+        libgfortran_version = preferred_libgfortran_version(platform, shards[gccboostrap_shard_idx])
+        cxxstring_abi = preferred_cxxstring_abi(platform, shards[gccboostrap_shard_idx])
+        concrete_platform = replace_cxxstring_abi(replace_libgfortran_version(platform, libgfortran_version), cxxstring_abi)
+    end
+    return concrete_platform
+end
+
+"""
+    get_concrete_platform(platform::Platform;
+                          preferred_gcc_version = nothing,
+                          preferred_llvm_version = nothing,
+                          compilers = nothing)
+
+Return the concrete platform for the given `platform` based on the GCC compiler
+ABI.  The set of shards is chosen by the keyword arguments (see [`choose_shards`](@ref)).
+"""
+function get_concrete_platform(platform::Platform;
+                               preferred_gcc_version = nothing,
+                               preferred_llvm_version = nothing,
+                               compilers = nothing)
+    shards = choose_shards(platform;
+                           preferred_gcc_version = preferred_gcc_version,
+                           preferred_llvm_version = preferred_llvm_version,
+                           compilers = compilers)
+    return get_concrete_platform(platform, shards)
+end
 
 """
     autobuild(dir::AbstractString, src_name::AbstractString,
@@ -575,19 +620,7 @@ function autobuild(dir::AbstractString,
         mkpath(build_path)
 
         shards = choose_shards(platform; extract_kwargs(kwargs, (:preferred_gcc_version,:preferred_llvm_version,:bootstrap_list,:compilers))...)
-        # We want to get dependencies that have exactly the same GCC ABI as the
-        # chosen compiler, otherwise we risk, e.g., to build in an environment
-        # with libgfortran3 a dependency built with libgfortran5.
-        # `concrete_platform` is needed only to setup the dependencies and the
-        # runner.  We _don't_ want the platform passed to `audit()` or
-        # `package()` to be more specific than it is.
-        concrete_platform = platform
-        gccboostrap_shard_idx = findfirst(x -> x.name == "GCCBootstrap" && x.target.arch == platform.arch && x.target.libc == platform.libc, shards)
-        if !isnothing(gccboostrap_shard_idx)
-            libgfortran_version = preferred_libgfortran_version(platform, shards[gccboostrap_shard_idx])
-            cxxstring_abi = preferred_cxxstring_abi(platform, shards[gccboostrap_shard_idx])
-            concrete_platform = replace_cxxstring_abi(replace_libgfortran_version(platform, libgfortran_version), cxxstring_abi)
-        end
+        concrete_platform = get_concrete_platform(platform, shards)
 
         prefix = setup_workspace(
             build_path,
