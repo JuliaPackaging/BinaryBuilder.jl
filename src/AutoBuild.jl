9035a1c23ef67d432039b91d0e4a10e7b3ef82f3
Mosè Giordano <mose@gnu.org>
2020-04-14 14:50:35 +0100

    Use GitHub Actions for CI

--- src/AutoBuild.jl
+++ src/AutoBuild.jl
@@ -587,8 +587,9 @@ function autobuild(dir::AbstractString,
                    kwargs...)
     @nospecialize
 
-    # If we're on CI and we're not verbose, schedule a task to output a "." every few seconds
-    if (haskey(ENV, "TRAVIS") || haskey(ENV, "CI")) && !verbose
+    # If we're on CI and we're not verbose, schedule a task to output a "." every few seconds.
+    # Exclude GitHub Actions, it wouldn't time out
+    if (haskey(ENV, "TRAVIS") || (haskey(ENV, "CI") && !haskey(ENV, "GITHUB_ACTIONS"))) && !verbose
         run_travis_busytask = true
         travis_busytask = @async begin
             # Don't let Travis think we're asleep...
@@ -813,7 +814,7 @@ function autobuild(dir::AbstractString,
         end
     end
 
-    if (haskey(ENV, "TRAVIS") || haskey(ENV, "CI")) && !verbose
+    if (haskey(ENV, "TRAVIS") || (haskey(ENV, "CI") && !haskey(ENV, "GITHUB_ACTIONS"))) && !verbose
         run_travis_busytask = false
         wait(travis_busytask)
         println()
