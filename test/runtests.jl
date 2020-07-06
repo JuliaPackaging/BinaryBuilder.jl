d9a96adef72c1f2e03d566de986708ef2883af8a
Mosè Giordano <giordano@users.noreply.github.com>
2020-07-07 00:15:20 +0100

    Add basic tests for JLL wrappers (#838)

--- test/runtests.jl
+++ test/runtests.jl
@@ -25,9 +25,42 @@ function with_libgfortran(f::Function)
     withenv(f, LIBPATH_var => libpath)
 end
 
+## Tests involing building packages and whatnot
+libfoo_products = [
+    LibraryProduct("libfoo", :libfoo),
+    ExecutableProduct("fooifier", :fooifier),
+]
+
+libfoo_make_script = raw"""
+cd ${WORKSPACE}/srcdir/libfoo
+make install
+install_license ${WORKSPACE}/srcdir/libfoo/LICENSE.md
+"""
+
+libfoo_cmake_script = raw"""
+mkdir ${WORKSPACE}/srcdir/libfoo/build && cd ${WORKSPACE}/srcdir/libfoo/build
+cmake -DCMAKE_INSTALL_PREFIX=${prefix} -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} ..
+make install
+install_license ${WORKSPACE}/srcdir/libfoo/LICENSE.md
+"""
+
+libfoo_meson_script = raw"""
+mkdir ${WORKSPACE}/srcdir/libfoo/build && cd ${WORKSPACE}/srcdir/libfoo/build
+meson .. -Dprefix=${prefix} --cross-file="${MESON_TARGET_TOOLCHAIN}"
+ninja install -v
+
+# grumble grumble meson!  Why do you go to all the trouble to build it properly
+# in `build`, then screw it up when you `install` it?!  Silly willy.
+if [[ ${target} == *apple* ]]; then
+    install_name_tool ${prefix}/bin/fooifier -change ${prefix}/lib/libfoo.0.dylib @rpath/libfoo.0.dylib
+fi
+install_license ${WORKSPACE}/srcdir/libfoo/LICENSE.md
+"""
+
 # Run all our tests
 include("basic.jl")
 include("building.jl")
 include("auditing.jl")
+include("jll.jl")
 include("wizard.jl")
 include("declarative.jl")
