8ef531de6a6e6819327d68397b9e412a66cc6ad3
Mosè Giordano <giordano@users.noreply.github.com>
2020-11-15 10:10:52 +0000

    Fix `versioninfo` and base it on BBB function (#965)

--- src/BinaryBuilder.jl
+++ src/BinaryBuilder.jl
@@ -3,7 +3,6 @@ using Libdl, LibGit2, Random, JSON
 using BinaryBuilderBase
 using ObjectFile
 using GitHub
-import InteractiveUtils
 using Pkg, Base.BinaryPlatforms, Pkg.Artifacts
 using ghr_jll
 
@@ -54,102 +53,9 @@ function __init__()
     end
 end
 
-function get_bb_version()
-    # Get BinaryBuilder.jl's version and git sha
-    version = Pkg.TOML.parsefile(joinpath(@__DIR__, "..", "Project.toml"))["version"]
-    try
-        # get the gitsha if we can
-        repo = LibGit2.GitRepo(dirname(@__DIR__))
-        gitsha = string(LibGit2.GitHash(LibGit2.GitCommit(repo, "HEAD")))
-        return VersionNumber("$(version)-git-$(gitsha[1:10])")
-    catch
-        try
-            # Settle for the treehash otherwise
-            env = Pkg.Types.Context().env
-            bb_uuid = Pkg.Types.UUID("12aac903-9f7c-5d81-afc2-d9565ea332ae")
-            treehash = bytes2hex(env.manifest[bb_uuid].tree_hash.bytes)
-            return VersionNumber("$(version)-tree-$(treehash[1:10])")
-        catch
-            # Something went so wrong, we can't get any of that.
-            return VersionNumber(version)
-        end
-    end
-end
-
-"""
-    versioninfo()
-
-Helper function to print out some debugging information
-"""
-function versioninfo()
-    @info("Julia versioninfo(): ")
-    InteractiveUtils.versioninfo()
-
-    @info("BinaryBuilder.jl version: $(get_bb_version())")
-
-    @static if Sys.isunix()
-        @info("Kernel version: $(readchomp(`uname -r`))")
-    end
-
-    # Dump if some important directories are encrypted:
-    @static if Sys.islinux()
-        print_enc(n, path) = begin
-            is_encrypted, mountpoint = is_ecryptfs(path)
-            if is_encrypted
-                @info("$n is encrypted on mountpoint $mountpoint")
-            else
-                @info("$n is NOT encrypted on mountpoint $mountpoint")
-            end
-        end
-
-        print_enc("pkg dir", dirname(@__FILE__))
-        print_enc("storage dir", storage_dir())
-    end
-
-    # Dump any relevant environment variables:
-    @info("Relevant environment variables:")
-    env_var_suffixes = [
-        "AUTOMATIC_APPLE",
-        "USE_SQUASHFS",
-        "STORAGE_DIR",
-        "RUNNER",
-        "ALLOW_ECRYPTFS",
-        "USE_CCACHE",
-    ]
-    for e in env_var_suffixes
-        envvar = "BINARYBUILDER_$(e)"
-        if haskey(ENV, envvar)
-            @info("  $(envvar): \"$(ENV[envvar])\"")
-        end
-    end
-
-    # Print out the preferred runner stuff here:
-    @info("Preferred runner: $(preferred_runner())")
-
-    # Try to run 'echo julia' in Linux x86_64 environment
-    @info("Trying to run `echo hello julia` within a Linux x86_64 environment...")
-
-    runner = preferred_runner()(
-        pwd();
-        cwd="/workspace/",
-        platform=Platform("x86_64", "linux"),
-        verbose=true
-    )
-    run_interactive(runner, `/bin/bash -c "echo hello julia"`)
-
-    # If we use ccache, dump the ccache stats
-    if BinaryBuilderBase.use_ccache
-        @info("ccache stats:")
-        runner = preferred_runner()(
-            pwd();
-            cwd="/workspace/",
-            platform=Platform("x86_64", "linux"),
-        )
-        run_interactive(runner, `/usr/bin/ccache -s`)
-    end
-    return nothing
-end
-
+get_bb_version() =
+    BinaryBuilderBase.get_bbb_version(@__DIR__, "12aac903-9f7c-5d81-afc2-d9565ea332ae")
+versioninfo() = BinaryBuilderBase.versioninfo(; name=@__MODULE__, version=get_bb_version())
 
 # Precompilation ahoy!
 # include("../deps/SnoopCompile/precompile/precompile_BinaryBuilder.jl")
