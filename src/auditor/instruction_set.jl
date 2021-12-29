d4eb035bd1b96303f08c124eb3906262e88dfb82
Kristoffer Carlsson <kcarlsson89@gmail.com>
2021-12-29 21:30:38 +0100

    improve performance of instruction set checker (#1140)
    * improve performance of instruction set checker
    
    * make work on 1.6

--- src/auditor/instruction_set.jl
+++ src/auditor/instruction_set.jl
@@ -24,8 +24,8 @@ binned by the mapping defined within `instruction_categories`.
 """
 function instruction_mnemonics(path::AbstractString, platform::AbstractPlatform)
     # The outputs we are calculating
-    counts = Dict(k => 0 for k in keys(instruction_categories))
-    mnemonics = String[]
+    counts = Dict{SubString{String}, Int}(k => 0 for k in keys(instruction_categories))
+    mnemonics = Set{SubString{String}}()
 
     ur = preferred_runner()(
         abspath(dirname(path));
@@ -44,15 +44,22 @@ function instruction_mnemonics(path::AbstractString, platform::AbstractPlatform)
     run_interactive(ur, `/bin/bash -c "$(objdump_cmd)"`; stdout=output, stderr=devnull)
     seekstart(output)
 
-    for line in eachline(output)
-        # First, ensure that this line of output is 3 fields long at least
-        fields = filter(x -> !isempty(strip(x)), split(line, '\t'))
-        if length(fields) < 3
-            continue
-        end
+    @time for line in eachline(output)
+        isempty(line) && continue
 
+        # First, ensure that this line of output is 3 fields long at least
+        @static if VERSION >= v"1.7.0-DEV.35"
+            count('\t', line) != 2 && continue
+        else
+            count(==('\t'), line) != 2 && continue
+        end
         # Grab the mnemonic for this line as the first word of the 3rd field
-        m = split(fields[3])[1]
+        idx = findlast('\t', line)
+        s = SubString(line, idx+1)
+        space = findfirst(' ', s)
+        space === nothing && (space = lastindex(s))
+        m = SubString(s, 1, space-1)
+
         push!(mnemonics, m)
 
         # For each mnemonic, find it in mnemonics_by_category, if we can, and
