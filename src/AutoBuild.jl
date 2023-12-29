8ee3915beee0dd9ea977657e51a1b138cdeffe80
Mosè Giordano <mose@gnu.org>
2023-12-29 21:42:34 +0100

    [AutoBuild] Forward `clang_use_lld` keyword argument to `preferred_runner`

--- src/AutoBuild.jl
+++ src/AutoBuild.jl
@@ -839,7 +839,7 @@ function autobuild(dir::AbstractString,
             compiler_wrapper_dir = joinpath(prefix, "compiler_wrappers"),
             src_name = src_name,
             shards = shards,
-            extract_kwargs(kwargs, (:preferred_gcc_version,:preferred_llvm_version,:compilers,:allow_unsafe_flags,:lock_microarchitecture))...,
+            extract_kwargs(kwargs, (:preferred_gcc_version,:preferred_llvm_version,:compilers,:allow_unsafe_flags,:lock_microarchitecture,:clang_use_lld))...,
         )
 
         # Set up some bash traps
