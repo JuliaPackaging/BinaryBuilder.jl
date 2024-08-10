4c33fff80391691ae84ff396f4af1705d1c2ca8b
Mosè Giordano <mose@gnu.org>
2024-08-10 11:15:12 +0200

    Make package loadable for Automerge in General Registry

--- src/BinaryBuilder.jl
+++ src/BinaryBuilder.jl
@@ -44,7 +44,7 @@ include("Declarative.jl")
 include("Logging.jl")
 
 function __init__()
-    if Base.thisminor(VERSION) >= v"1.8"
+    if Base.thisminor(VERSION) >= v"1.8" && get(ENV, "JULIA_REGISTRYCI_AUTOMERGE", "false") != "true"
         error("""
         BinaryBuilder supports only Julia v1.7.
         Contribute to JuliaPackaging/JLLPrefixes.jl#6 (<https://github.com/JuliaPackaging/JLLPrefixes.jl/issues/6>)
